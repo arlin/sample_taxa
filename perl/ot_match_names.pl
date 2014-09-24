0fb8b12c412b2f805a8c5bdf28a383f0479387d0
Arlin Stoltzfus <arlin@umd.edu>
2014-09-24 09:40:10 -0400

    defaults to approx match = false; added fuzzy flag; safer handling of user's input string of names

--- perl/ot_match_names.pl
+++ perl/ot_match_names.pl
@@ -7,19 +7,20 @@ ot_match_names.pl - get ottIds for named species using OpenTree web services
 
 =head1 SYNOPSIS
 
-    ot_match_names.pl --names "<name1>[ ,<name2>]*" --file FILE
+    ot_match_names.pl --names "<name1>[ ,<name2>]*" --file FILE 
 
+    --fuzzy				use approximate matching (off by default)
     --help, --?         print help message
 
 Where I<FILE> is an optional file name to dump output for troubleshooting. 
 
 Examples:
 
-    ot_match_names.pl --names '"Panthera tigris","Sorex araneus","Erinaceus europaeus"'  
+    ot_match_names.pl --names "Panthera tigris, Sorex araneus, Erinaceus europaeus"  
 
 =head1 DESCRIPTION
 
-This returns a compact list of ottId values ("val1,val2,val3"). 
+This returns a compact list of ottId values ("val1,val2,val3").  By default, approximate matching is turned off, both for purposes of speed, and to prevent unintended results.  If you want to use approximate matching, set the fuzzy flag. 
 
 =head1 KNOWN ISSUES
 
@@ -38,12 +39,14 @@ use Pod::Usage;
 # process command-line arguments 
 #
 my $email = "arlin\@umd.edu"; 
+my $fuzzy = 0; 
 
 my ( $names, $help ); 
 my $outfile = "match_result.json";
 GetOptions(
     "email=s" => \$email,   
     "names=s" => \$names, 
+    "fuzzy!" => \$fuzzy, 
     "file=s" => \$outfile, 
     "help|?" => \$help
     ) or pod2usage( "Invalid command-line options." );
@@ -52,12 +55,20 @@ pod2usage() if defined( $help );
 my $script = $0; 
 $script =~ s#^.*/##; 
 
+# service demands quoted strings like {"names":["Aster", "Symphyotrichum", "Erigeron"]}
+# which the user may or may not have supplied already, 
+# so we will strip the user's name list, rebuild with double-quotes around each name
+
+my @names = split( /, */, $names ); 
+s/^[\'\"]*|[\'\"]*$//g for @names; 
+$names = sprintf( "\"%s\"", join( '","', @names ) ); 
+
 # construct the URL 
 #
 my $base_url = "http://devapi.opentreeoflife.org/v2/tnrs/match_names"; 
 my $header = "Content-Type:application/json"; 
-# json: {"names":["Aster","Symphyotrichum","Erigeron","Barnadesia"]}
-my $body = sprintf( "{\"names\":[%s]}", $names ); 
+my $body = sprintf( "{\"names\":[%s], \"do_approximate_matching\" : %s }", $names, ( $fuzzy ? "true" : "false" ) ); 
+
 my $command = sprintf( "curl -X POST $base_url -H \"$header\" -d \'$body\'" ); 
 
 # debug 
